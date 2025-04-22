e0d8e94b2d2f9ed34a8d9ddc0e4861313f24f525
hariharan <samkaran82@gmail.com>
2025-04-22 08:59:23 -0700

    fix css import error 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import "./Component/Hero/Hero.css"
+import "./Component/Hiring/Hiring.css"
+import "./Component/Navigation/Navbar.css"
+import "./Component/Skillset/Skillset.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
